d21e8b18c2ec58a7bb0410e2d02b21b5065a7701
smd <smd78@list.ru>
2023-06-10 14:43:08 +0300

    add-css-border-sidebar

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// let arr = ['слива','дыня','123', {
+//     wer:10,
+//     ter:20
+// },'234'];
+
+// console.log(arr[0,2]);
+
+// let sa = confirm('are you here?');
+// let ans = +prompt('Вам есть 18?','18');
+// console.log(typeof(ans));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+console.log(2*4 === '8');
